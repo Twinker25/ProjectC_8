a29309376a310c13a0987ea19b1fd1f59cdbd833
Twinker25 <shashokd@gmail.com>
2023-04-20 18:26:43 +0300

    Fix Calculator

--- ProjectC_8/Program.cs
+++ ProjectC_8/Program.cs
@@ -24,6 +24,7 @@
             double rez;
             do
             {
+                MyDelegate myDelegate = null;
                 Console.Write("\nNum 1: ");
                 double ch1 = int.Parse(Console.ReadLine());
                 Console.Write("Num 2: ");
@@ -33,29 +34,23 @@
                 switch (ch)
                 {
                     case 1:
-                        MyDelegate myDelegate1 = Add;
-                        rez = myDelegate1(ch1, ch2);
-                        Console.WriteLine("Result: {0}", rez);
+                        myDelegate = Add;
                         break;
                     case 2:
-                        MyDelegate myDelegate2 = Sub;
-                        rez = myDelegate2(ch1, ch2);
-                        Console.WriteLine("Result: {0}", rez);
+                        myDelegate = Sub;
                         break;
                     case 3:
-                        MyDelegate myDelegate3 = Mult;
-                        rez = myDelegate3(ch1, ch2);
-                        Console.WriteLine("Result: {0}", rez);
+                        myDelegate = Mult;
                         break;
                     case 4:
-                        MyDelegate myDelegate4 = Div;
-                        rez = myDelegate4(ch1, ch2);
-                        Console.WriteLine("Result: {0}", rez);
+                        myDelegate = Div;
                         break;
                     default:
                         Console.WriteLine("Error!");
                         break;
                 }
+                rez = myDelegate(ch1, ch2);
+                Console.WriteLine("Result: {0}", rez);
             } while (true);
         }
     }
